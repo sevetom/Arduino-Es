2c3b4be6cda72d02475d528ee79f0efe1d681345
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-09-27 11:23:50 +0200

    Second ex

---
+++ Lab1/blinking_external_led/blinking_external_led.ino
@@ -0,0 +1,15 @@
+#define LED_PIN 13
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("ON");
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("OFF");
+  delay(1000);
+}
