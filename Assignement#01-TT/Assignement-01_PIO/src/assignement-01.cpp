a7a9904598aec62c22fa43d7a9b6be4499024075
Tommaso <tommaso.ceredi@studio.unibo.it>
2023-10-10 15:26:58 +0200

    debugging buttons

--- Assignement#01-TT/Assignement-01_PIO/src/assignement-01.cpp
+++ Assignement#01-TT/Assignement-01_PIO/src/assignement-01.cpp
@@ -38,6 +38,9 @@ typedef enum {
 volatile gameState state;
 buttonLed buttonLedArr[SIZE];
 volatile int currentTurn;
+int score;
+float potentiometerValue;
+int t1, t2, t3;
 
 void loop();
 void redFade(int fadeAmount);
@@ -47,10 +50,15 @@ void startLeds();
 void switchGreens(bool state);
 int randomLedOrder(int *arr);
 bool isPresent(int *arr, int num);
+void setT();
 
 void setup() {
+    Serial.begin(115200);
     state = starting;
     currentTurn = 0;
+    score = 0;
+    potentiometerValue = 1.0;
+    setT();
     srand(time(NULL));
     buttonLedArr[0] = { BUTTON1, GREENLED1, UNDEFINED };
     buttonLedArr[1] = { BUTTON2, GREENLED2, UNDEFINED };
@@ -62,9 +70,8 @@ void setup() {
         pinMode(buttonLedArr[i].ledPin, OUTPUT);
     }
     pinMode(REDLED, OUTPUT);
-    Timer1.initialize(15000000);
-    Timer1.attachInterrupt(sleep);
-    Serial.begin(115200);
+    //Timer1.initialize(15000000);
+    //Timer1.attachInterrupt(sleep);
 }
 
 void loop() {
@@ -111,27 +118,34 @@ void buttonPressed() {
     }
     if (state == waitingPlayer) {
         for (int i = 0; i < SIZE; i++) {
-            if (digitalRead(buttonLedArr[i].buttonPin) == HIGH && digitalRead(buttonLedArr[i].ledPin == LOW)) {
+            noInterrupts();
+            if (digitalRead(buttonLedArr[i].buttonPin) == HIGH && buttonLedArr[i].turn != UNDEFINED) {
                 if (buttonLedArr[i].turn == currentTurn) {
                     currentTurn--;
+                    buttonLedArr[i].turn = UNDEFINED;
                     digitalWrite(buttonLedArr[i].ledPin, HIGH);
-                    if (currentTurn == UNDEFINED) {
+                    if (currentTurn < 0) {
+                        score++;
+                        Serial.print("New point! Score: ");
+                        Serial.println(score);
+                        potentiometerValue = potentiometerValue * 0.9;
+                        setT();
                         state = waitingLed;
                     }
                 } else {
                     state = lost;
                 }
             }
+            interrupts();
+            break;
         }
     }
     delay(500);
 }
 
 void losingCase() {
+    score = 0;
     switchGreens(false);
-    for (int i = 0; i < SIZE; i++) {
-        buttonLedArr[i].turn = UNDEFINED;
-    }
     digitalWrite(REDLED, HIGH);
     delay(1000);
     digitalWrite(REDLED, LOW);
@@ -140,18 +154,25 @@ void losingCase() {
 
 void startLeds() {
     switchGreens(true);
+    delay(t1);
     int arr[SIZE];
     for (int i = 0; i < SIZE; i++) {
         arr[i] = UNDEFINED;
+        buttonLedArr[i].turn = UNDEFINED;
     }
     for (int i=0; i<SIZE; i++) {
-        delay(DELAY);
+        delay(t2/4);
         arr[i] = randomLedOrder(arr);
         digitalWrite(buttonLedArr[arr[i]].ledPin, LOW);
         buttonLedArr[arr[i]].turn = i;
     }
     currentTurn = SIZE-1;
     state = waitingPlayer;
+    for (int i = 0; i<SIZE; i++){
+        Serial.print(arr[i]);
+        Serial.print(" ");
+    }
+    Serial.println("");
 }
 
 void switchGreens(bool state) {
@@ -161,9 +182,9 @@ void switchGreens(bool state) {
 }
 
 int randomLedOrder(int *arr) {
-    int randomNumber = rand() % 4;
+    int randomNumber = randomInt(0, 4);
     while (isPresent(arr, randomNumber)) {
-        randomNumber = rand() % 4;
+        randomNumber = randomInt(0, 4);
     }
     return randomNumber;
 }
@@ -182,11 +203,15 @@ void sleep() {
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_enable();
     for (int i = 0;i < SIZE; i++) {
+        disableInterrupt(buttonLedArr[i].buttonPin);
+        pinMode(buttonLedArr[i].buttonPin, INPUT);
         enableInterrupt(buttonLedArr[i].buttonPin, wakeUp, CHANGE);
     }
     sleep_mode();
     sleep_disable();
     for (int i = 0;i < SIZE; i++) {
+        disableInterrupt(buttonLedArr[i].buttonPin);
+        pinMode(buttonLedArr[i].buttonPin, INPUT);
         enableInterrupt(buttonLedArr[i].buttonPin, buttonPressed, CHANGE);
     }
     Serial.println("Waking up");
@@ -194,4 +219,20 @@ void sleep() {
 
 void wakeUp() {
     state = starting;
+}
+
+int randomInt(float min, float max) {
+    return (int)(min + (max - min) * (float)rand() / (float)RAND_MAX);
+}
+
+void setT() {
+    t1 = (int)randomInt((float)1000*potentiometerValue, (float)3000*potentiometerValue); // time to start the game
+    t2 = (int)randomInt((float)3000*potentiometerValue, (float)5000*potentiometerValue); // time to turn on the leds
+    t3 = (int)randomInt((float)4000*potentiometerValue, (float)6000*potentiometerValue); // time to wait for the player
+    Serial.print("t1: ");
+    Serial.print(t1);
+    Serial.print(" | t2: ");
+    Serial.print(t2);
+    Serial.print(" | t3: ");
+    Serial.println(t3);
 }
