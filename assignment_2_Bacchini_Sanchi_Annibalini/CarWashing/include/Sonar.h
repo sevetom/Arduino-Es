471a444577661a25367440a1dc15f9dab8f54b8d
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-11-18 10:35:57 +0100

    modificato Sonar.h per sonar a disposizione

--- assignment_2_Bacchini_Sanchi_Annibalini/CarWashing/include/Sonar.h
+++ assignment_2_Bacchini_Sanchi_Annibalini/CarWashing/include/Sonar.h
@@ -2,9 +2,11 @@
 #define __SONAR__
 class Sonar {
     public:
-        Sonar(int pin);
+        Sonar(int pinIN, int pinOUT, float velocitaSuono);
         float getDistance();
     protected:
-        int pin; 
+        int pinIn;
+        int pinOut;
+        float velocitaSuono; 
 };
 #endif
