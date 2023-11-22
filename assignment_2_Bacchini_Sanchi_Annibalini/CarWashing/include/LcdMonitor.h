a05084b2a8261d6a6e10bb935deafe54e172cb65
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-11-22 08:51:29 +0100

    moved assignment_Bacchini_Sanchi_Annibalini to another repo

--- assignment_2_Bacchini_Sanchi_Annibalini/CarWashing/include/LcdMonitor.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MONITOR__
-#define __MONITOR__
-#include <LiquidCrystal_I2C.h> 
-class LcdMonitor {
-    public:
-        LcdMonitor(int rows, int columns);
-        void setAndPrint(char* string, int xposition, int yposition);
-};
-#endif
